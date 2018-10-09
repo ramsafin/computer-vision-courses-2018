0d24d2e86178861ae2cf72e7c0994d619ad0d45e
Ramil Safin <RamilSafNab1996@gmail.com>
2018-10-09 18:14:49 +0300

    Fix mistake in find favorite task: use cross-correlation

--- 1st/find_favorite.m
+++ 1st/find_favorite.m
@@ -1,13 +1,13 @@
 % Finds the location of the template in the image.
 function [x_index, y_index] = find_favorite(t, I)
 
-    % calculate the normalized cross-correlation
-    c = normxcorr2(t, I);
+    % calculate the cross-correlation
+    c = xcorr2(t, I);
     
     % Find the peak in cross-correlation.
     % For this the max values indices are found
     % in the correlation matrix 'c'.
-    [x_peak, y_peak] = find(c == max(c(:)));
+    [y_peak, x_peak] = find(c == max(c(:)));
     
     % consider the template's paddings
     x_index = x_peak - size(t, 2) + 1;
